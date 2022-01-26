f1586b54b4560bf113ec411a2845c2f99d2dea06
Natalyasem <seraya.93@inbox.ru>
2022-01-26 09:25:53 +0300

    start ui-kit

---
+++ src/pages/admin/ui-kit.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="font-bold">
+    UI KIT
+  </div>
+  <!-- КНОПКИ -->
+
+  <button
+    type="button"
+    class="app-button-main"
+  >
+    Main button
+  </button>
+
+  <!--  TODO: Шрифт-->
+  <!-- TODO: Иконки-->
+</template>
+
+<script>
+export default {
+  name: 'ui-kit'
+}
+</script>
+
+<style>
+.app-button-main {
+  @apply text-white bg-green-1 py-3 px-6;
+  border-radius: 50px;
+}
+
+
+.app-button-secondary {
+
+}
+
+.app-button-tertiary {
+
+}
+</style>
