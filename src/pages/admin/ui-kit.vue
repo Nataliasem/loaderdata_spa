4eba2e1667e0468bdf6677f666ca6f0efbf13bae
Natalyasem <seraya.93@inbox.ru>
2022-01-29 18:52:25 +0300

    update uikit

--- src/pages/admin/ui-kit.vue
+++ src/pages/admin/ui-kit.vue
@@ -1,104 +1,127 @@
 <template>
-  <!-- КНОПКИ -->
-  <div class="button-section flex justify-between">
-    <div class="font-bold text-size-42">
-      BUTTONS
+  <div class="app-ui-kit space-y-40">
+    <!-- КНОПКИ -->
+    <div class="flex justify-center space-x-20">
+      <div class="font-bold text-size-42 w-1/4">
+        BUTTONS
+      </div>
+
+      <div class="flex flex-grow-1 space-x-10 w-3/4">
+        <!-- BUTTON-MAIN -->
+        <div class="flex flex-col space-y-6">
+          <button
+            type="button"
+            class="app-button-main"
+          >
+            Main default
+          </button>
+          <button
+            type="button"
+            class="app-button-main hover"
+          >
+            Main hover
+          </button>
+          <button
+            type="button"
+            class="app-button-main active"
+          >
+            Main active
+          </button>
+          <button
+            type="button"
+            class="app-button-main disabled"
+          >
+            Main disabled
+          </button>
+        </div>
+
+        <!-- BUTTON-SECONDARY -->
+        <div class="flex flex-col space-y-6">
+          <button
+            type="button"
+            class="app-button-secondary"
+          >
+            Secondary default
+          </button>
+          <button
+            type="button"
+            class="app-button-secondary hover"
+          >
+            Secondary hover
+          </button>
+          <button
+            type="button"
+            class="app-button-secondary active"
+          >
+            Secondary active
+          </button>
+          <button
+            type="button"
+            class="app-button-secondary disabled"
+          >
+            Secondary disabled
+          </button>
+        </div>
+
+        <!-- BUTTON-ICON -->
+        <div class="flex flex-col space-y-6">
+          <button
+            type="button"
+            class="app-button-icon text-size-30"
+          >
+            &#9734;
+          </button>
+          <button
+            type="button"
+            class="app-button-icon text-size-30 hover"
+          >
+            &#9734;
+          </button>
+          <button
+            type="button"
+            class="app-button-icon text-size-30 active"
+          >
+            &#9734;
+          </button>
+          <button
+            type="button"
+            class="app-button-icon text-size-30 disabled"
+          >
+            &#9734;
+          </button>
+        </div>
+      </div>
     </div>
 
-    <div class="flex flex-grow-1 space-x-10">
-      <!-- BUTTON-MAIN -->
-      <div class="flex flex-col space-y-6">
-        <button
-          type="button"
-          class="app-button-main"
-        >
-          Main default
-        </button>
-        <button
-          type="button"
-          class="app-button-main hover"
-        >
-          Main hover
-        </button>
-        <button
-          type="button"
-          class="app-button-main active"
-        >
-          Main active
-        </button>
-        <button
-          type="button"
-          class="app-button-main disabled"
-        >
-          Main disabled
-        </button>
+    <!-- ИКОНКИ -->
+    <div class="flex justify-center space-x-20">
+      <div class="font-bold text-size-42 w-1/4">
+        ICONS
       </div>
 
-      <!-- BUTTON-SECONDARY -->
-      <div class="flex flex-col space-y-6">
-        <button
-          type="button"
-          class="app-button-secondary"
-        >
-          Secondary default
-        </button>
-        <button
-          type="button"
-          class="app-button-secondary hover"
-        >
-          Secondary hover
-        </button>
-        <button
-          type="button"
-          class="app-button-secondary active"
-        >
-          Secondary active
-        </button>
-        <button
-          type="button"
-          class="app-button-secondary disabled"
-        >
-          Secondary disabled
-        </button>
-      </div>
-
-      <!-- BUTTON-ICON -->
-      <div class="flex flex-col space-y-6">
-        <button
-          type="button"
-          class="app-button-icon text-size-30"
-        >
-          &#9734;
-        </button>
-        <button
-          type="button"
-          class="app-button-icon text-size-30 hover"
-        >
-          &#9734;
-        </button>
-        <button
-          type="button"
-          class="app-button-icon text-size-30 active"
-        >
-          &#9734;
-        </button>
-        <button
-          type="button"
-          class="app-button-icon text-size-30 disabled"
-        >
-          &#9734;
-        </button>
+      <div class="flex w-3/4 space-x-8">
+        <app-icon-user class="text-green-1" />
+        <app-icon-login class="text-green-1" />
+        <app-icon-logout class="text-green-1" />
       </div>
     </div>
+    <!-- TODO: Шрифт-->
   </div>
-
-  <!-- TODO: Шрифт-->
-  <!-- TODO: Иконки-->
 </template>
 
+
 <script>
+import AppIconUser from '/src/components/icons/app-icon-user.vue'
+import AppIconLogout from '/src/components/icons/app-icon-logout.vue'
+import AppIconLogin from '/src/components/icons/app-icon-login.vue'
+
 export default {
   name: 'ui-kit',
+  components: {
+    AppIconUser,
+    AppIconLogout,
+    AppIconLogin
+  },
   middleware: 'auth'
 }
 </script>
