f7689cfcf54150580b9b4a7ebd62d102ba5f42bd
Natalyasem <seraya.93@inbox.ru>
2022-01-29 17:39:30 +0300

    put middleware in separate file

--- src/constants.js
+++ src/constants.js
@@ -2,3 +2,22 @@ export const BASE_URL = {
     HOST: 'http://localhost',
     PORT: '8080'
 }
+
+export const ROLES = {
+    OWNER: {
+        ID: 1,
+        AUTHORITIES: ['manage_users', 'manage_settings', 'view_all_pages', 'view_some_pages']
+    },
+    ADMIN: {
+        ID: 2,
+        AUTHORITIES: ['manage_settings', 'view_all_pages', 'view_some_pages']
+    },
+    ADVANCED_USER: {
+        ID: 3,
+        AUTHORITIES: ['view_all_pages', 'view_some_pages']
+    },
+    DEFAULT_USER: {
+        ID: 4,
+        AUTHORITIES: ['view_some_pages']
+    }
+}
