38dab93482a927a072e2aa1735f01892a5a60394
Natalyasem <seraya.93@inbox.ru>
2022-01-29 18:52:08 +0300

    add icons

---
+++ src/components/icons/app-icon-login.vue
@@ -0,0 +1,48 @@
+<template>
+  <svg
+    :width="width"
+    :height="height"
+    viewBox="0 0 22 22"
+    fill="none"
+    stroke-width="2"
+    stroke="currentColor"
+    stroke-linecap="round"
+    stroke-linejoin="round"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <path
+      d="M15 3H19C19.5304 3 20.0391 3.21071 20.4142 3.58579C20.7893 3.96086 21 4.46957 21 5V19C21 19.5304 20.7893 20.0391 20.4142 20.4142C20.0391 20.7893 19.5304 21 19 21H15"
+    />
+    <path
+      d="M10 17L15 12L10 7"
+    />
+    <path
+      d="M15 12H3"
+    />
+  </svg>
+</template>
+
+<script>
+export default {
+  name: 'app-icon-login',
+  props: {
+    /**
+     * Ширина
+     * @type {number}
+     */
+    width: {
+      type: Number,
+      default: 24
+    },
+
+    /**
+     * Высота
+     * @type {number}
+     */
+    height: {
+      type: Number,
+      default: 24
+    }
+  }
+}
+</script>
---
+++ src/components/icons/app-icon-logout.vue
@@ -0,0 +1,48 @@
+<template>
+  <svg
+    :width="width"
+    :height="height"
+    viewBox="0 0 22 22"
+    fill="none"
+    stroke-width="2"
+    stroke="currentColor"
+    stroke-linecap="round"
+    stroke-linejoin="round"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <path
+      d="M9 21H5C4.46957 21 3.96086 20.7893 3.58579 20.4142C3.21071 20.0391 3 19.5304 3 19V5C3 4.46957 3.21071 3.96086 3.58579 3.58579C3.96086 3.21071 4.46957 3 5 3H9"
+    />
+    <path
+      d="M16 17L21 12L16 7"
+    />
+    <path
+      d="M21 12H9"
+    />
+  </svg>
+</template>
+
+<script>
+export default {
+  name: 'app-icon-logout',
+  props: {
+    /**
+     * Ширина
+     * @type {number}
+     */
+    width: {
+      type: Number,
+      default: 24
+    },
+
+    /**
+     * Высота
+     * @type {number}
+     */
+    height: {
+      type: Number,
+      default: 24
+    }
+  }
+}
+</script>
