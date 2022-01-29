5cffd2a1c3d80edbb8af07752dc0657d97b39ce9
Natalyasem <seraya.93@inbox.ru>
2022-01-29 18:52:20 +0300

    update navbar

--- src/components/layouts/app-navbar.vue
+++ src/components/layouts/app-navbar.vue
@@ -1,16 +1,59 @@
 <template>
   <div class="app-navbar">
-    <span class="logo">Loader<span class="text-secondary">Data</span></span>
+    <!-- ЛОГОТИП-->
+    <div
+      class="logo"
+      @click="backToHomePage"
+    >
+      Loader<span class="text-secondary">Data</span>
+    </div>
+
     <div class="flex space-x-5">
-      <div>Аккаунт</div>
-      <div>Выход</div>
+      <!-- АККАУНТ ПОЛЬЗОВАТЕЛЯ-->
+      <button type="button">
+        <app-icon-user class="inline-block" />
+      </button>
+
+      <!-- ВЫХОД ИЗ СИСТЕМЫ-->
+      <button
+        type="button"
+        @click="logout"
+      >
+        <app-icon-logout class="inline-block" />
+      </button>
     </div>
   </div>
 </template>
 
 <script>
+import AppIconUser from '/src/components/icons/app-icon-user.vue'
+import AppIconLogout from '/src/components/icons/app-icon-logout.vue'
+
 export default {
-  name: 'app-navbar'
+  name: 'app-navbar',
+  components: {
+    AppIconUser,
+    AppIconLogout
+  },
+  methods: {
+    /**
+     * Вернуться на домашнюю страницу
+     * @returns {void}
+     */
+    backToHomePage() {
+      this.$router.push('/')
+    },
+
+    /**
+     * Выйти из системы
+     * @returns {void}
+     */
+    logout() {
+      this.$store.state.user = null
+
+      this.backToHomePage()
+    }
+  }
 }
 </script>
 
@@ -20,6 +63,6 @@ export default {
 }
 
 .app-navbar .logo {
-  @apply font-bold text-size-30 text-primary;
+  @apply font-bold text-size-30 text-primary cursor-pointer;
 }
 </style>
