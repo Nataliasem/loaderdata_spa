e271688dfe5c165f208409f6931100ad335c097d
Natalyasem <seraya.93@inbox.ru>
2022-02-01 22:47:35 +0300

    update deps, fix eslint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,7 +2,8 @@ module.exports = {
   root: true,
   env: {
     jest: true,
-    es2021: true
+    es2021: true,
+    node: true
   },
   extends: [
     'plugin:vue/vue3-recommended',
@@ -47,4 +48,4 @@ module.exports = {
       }
     ]
   }
-}
+};
