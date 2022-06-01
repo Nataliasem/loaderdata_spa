748221b91cb5bb70b4c5c90ae0a708254da7dc54
your username <n.syomka@isoit.ru>
2022-06-01 12:10:19 +0300

    fixed

--- config.js
+++ config.js
@@ -1,7 +1,6 @@
 // Удалить, когда WebStorm начнёт поддерживать алиасы Vite
 // REF: https://stackoverflow.com/questions/71613474/cannot-resolve-directory-in-webstorm-for-a-vue-file
 
-// eslint-disable-next-line @typescript-eslint/no-var-requires
 const path = require('path')
 
 module.exports = {
