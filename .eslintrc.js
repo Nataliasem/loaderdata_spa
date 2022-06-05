1916e0cc3e1a119e94d9a5ea18bf646c3739965e
your username <n.syomka@isoit.ru>
2022-06-05 18:23:20 +0300

    TypeScript Eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,8 +3,13 @@ module.exports = {
     browser: true,
     node: true
   },
+  parser: 'vue-eslint-parser',
+  parserOptions: {
+    parser: '@typescript-eslint/parser'
+  },
   extends: [
     'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
     'plugin:vue/recommended',
     'plugin:promise/recommended',
     'prettier'
