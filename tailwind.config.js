29bd6f529c29034d79b144541409cf55494c1baa
Natalyasem <seraya.93@inbox.ru>
2022-06-21 11:04:53 +0300

    update colours in tw-config

--- tailwind.config.js
+++ tailwind.config.js
@@ -60,30 +60,14 @@ module.exports = {
       'white': '#fff',
 
       /* THEME COLORS */
-      'primary': '#092C4C',
-      'secondary': '#F2994A',
-      'info': '#2F80ED',
-      'success': '#27AE60',
-      'warning': '#E2B93B',
-
-      'red-1': '#EB5757',
-      'red-2': '#FFD8D8',
-      'red-3': '#FFF5F5',
-
-      'green-1': '#0B8C56',
-      'green-2': '#11613F',
-      'green-3': '#3da378',
-
-      'black-1': '#000000',
-      'black-2': '#1D1D1D',
-      'black-3': '#282828',
-
-      'gray-1': '#333333',
-      'gray-2': '#4F4F4F',
-      'gray-3': '#828282',
-      'gray-4': '#BDBDBD',
-      'gray-5': '#E0E0E0',
-      'gray-6': '#F5F5F5'
+      'blue-1': '#2d3748',
+      'blue-2': '#2C5282',
+      'blue-3': '#2d9cdb',
+      'grey-1': '#edf2f7',
+      'grey-2': '#f7fafc',
+      'yellow': '#fdc107',
+      'green': '#219553',
+      'red': '#fc8181'
     }
   }
 }
