649b4749b0d68f3b179beb0392ff2aa468e87204
Natalyasem <seraya.93@inbox.ru>
2022-06-22 16:18:18 +0300

    persisted pinia with locale storage

--- src/main.js
+++ src/main.js
@@ -1,12 +1,24 @@
-import { createApp } from 'vue'
+import { createApp, watch } from 'vue'
 import { createPinia } from 'pinia'
 import App from './App.vue'
 import router from './router/index.js'
 import './main.css'
 
+const pinia = createPinia()
+
+if (localStorage.getItem('userState')) {
+  pinia.state.value.user = JSON.parse(localStorage.getItem('userState'))
+}
+
+watch(
+  () => pinia.state.value.user,
+  (state) => localStorage.setItem('userState', JSON.stringify(state)),
+  { deep: true }
+)
+
 const app = createApp(App)
 
 app.use(router)
-app.use(createPinia())
+app.use(pinia)
 
 app.mount('#app')
