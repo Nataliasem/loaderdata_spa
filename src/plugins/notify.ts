9c398ccd7adc4b426073236f4215e48f3d86dd25
Natalia <seraya.93@inbox.ru>
2022-07-08 17:21:13 +0300

    удалена библиотека vue-toast-notification

--- src/plugins/notify.ts
+++ src/plugins/notify.ts
@@ -1,24 +1,44 @@
-import { useToast } from 'vue-toast-notification'
-import 'vue-toast-notification/dist/theme-sugar.css'
+import './notify.css'
 
-const $toast = useToast({
-  position: 'top-right'
-})
+const showAlert = (message: string, type = 'default') => {
+  if (!message) {
+    return
+  }
+
+  const alertElement = document.createElement('div')
+  alertElement.classList.add('ld-alert', 'fade-in', `ld-alert-${type}`)
+  alertElement.innerText = message
+
+  let alertWrapper = document.getElementById('ld-alert-wrapper')
+  if (!alertWrapper) {
+    alertWrapper = document.createElement('div')
+    alertWrapper.id = 'ld-alert-wrapper'
+    document.body.appendChild(alertWrapper)
+  }
+
+  alertWrapper.appendChild(alertElement)
+
+  const clearDom = () => {
+    alertElement.remove()
+
+    const alerts = document.querySelectorAll('.ld-alert')
+
+    if (alertWrapper && alerts.length === 0) {
+      alertWrapper.remove()
+    }
+  }
+
+  setTimeout(() => clearDom(), 3000)
+}
 
 export default {
   default(message: string) {
-    $toast.default(message)
-  },
-  info(message: string) {
-    $toast.info(message)
+    showAlert(message, 'default')
   },
   success(message: string) {
-    $toast.success(message)
-  },
-  warning(message: string) {
-    $toast.warning(message)
+    showAlert(message, 'success')
   },
   error(message: string) {
-    $toast.error(message)
+    showAlert(message, 'error')
   }
 }
