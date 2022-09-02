e951ac28e073aec2e45795b25bfb570887bf4e1b
natalya_semka <seraya.93@inbox.ru>
2022-09-02 22:05:24 +0300

    wip tests

--- src/pages/account/account-page.test.ts
+++ src/pages/account/account-page.test.ts
@@ -4,25 +4,94 @@ import {
   enableAutoUnmount,
   VueWrapper
 } from '@vue/test-utils'
-// import AccountPage from '~/pages/account/account-page'
+
 import { describe, expect, it, vi, afterEach } from 'vitest'
+
+// Типы
 import { User } from '~/types/main'
+
+// Компоненты
+// import AccountPage from '~/pages/account/account-page'
+import UserPreview from '~/components/user/user-preview.vue'
+import UserEdit from '~/components/user/user-edit.vue'
+import { ref, computed, Ref, onMounted } from 'vue'
+
+// API и утилиты
 import usersApi from '~/api/users'
+import notify from '~/plugins/notify'
+
 enableAutoUnmount(afterEach)
 
 const AccountPage = {
-  template: `
-  <div class="account-page">
-   <div v-if="isLoading" class="ld-loader">Загрузка</div>
-   
-   <template v-else>
-    <div v-if="emptyUser" class="data-error">
-      Данные не загрузились. Попробуйте обновить страницу
-    </div>
+  components: {
+    UserPreview,
+    UserEdit
+  },
+  setup() {
+    const isEditing = ref(false)
 
-    <user-details v-else :user=":user" @update="updateUser" />
-   </template>
-  </div>
+    // id пользователя из стора
+    const id = '4905bc62-cfdb-40e7-b9b8-15de8b83f888'
+
+    // composable
+    const useUser = (id: string) => {
+      const isLoading = ref(true)
+      const user: Ref<User | null> = ref(null)
+
+      const isUserEmpty = computed(() => {
+        return !user.value
+      })
+
+      const loadUser = () => {
+        isLoading.value = true
+
+        usersApi
+          .loadUser(id)
+          .then((userResponse) => (user.value = userResponse))
+          .catch((error) => notify.error(error))
+          .finally(() => (isLoading.value = false))
+      }
+
+      const updateUser = () => {
+        return true
+      }
+
+      onMounted(() => {
+        loadUser()
+      })
+
+      return {
+        isLoading,
+        user,
+        isUserEmpty,
+        loadUser,
+        updateUser
+      }
+    }
+
+    const { isLoading, user, isUserEmpty, updateUser } = useUser(id)
+
+    return {
+      isLoading,
+      user,
+      isUserEmpty,
+      updateUser,
+      isEditing
+    }
+  },
+  template: `
+    <div class="account-page">
+    <div v-if="isLoading" class="ld-loader">Загрузка</div>
+
+    <template v-else>
+      <div v-if="isUserEmpty" class="data-error">
+        Данные не загрузились. Попробуйте обновить страницу
+      </div>
+
+      <user-edit v-else-if="isEditing" :user="user" @update="updateUser" />
+      <user-preview v-else :user="user" @update="updateUser" />
+    </template>
+    </div>
   `
 }
 
@@ -46,7 +115,8 @@ describe('AccountPage.vue', () => {
 
   const findLoader = () => wrapper.find('.ld-loader')
   const findDataError = () => wrapper.find('.data-error')
-  const findUserDetails = () => wrapper.findComponent({ name: 'UserDetails' })
+  const findUserEdit = () => wrapper.findComponent({ name: 'UserEdit' })
+  const findUserPreview = () => wrapper.findComponent({ name: 'UserPreview' })
 
   it('загружает данные пользователя', async () => {
     wrapper = componentFactory()
@@ -56,22 +126,26 @@ describe('AccountPage.vue', () => {
       .mockResolvedValue(mockUser)
       .mockRejectedValue(new Error('Пользователь не загрузился'))
 
-    // отправляем запрос на загрузку
-    // await flushPromises()
-    await usersApi.loadUser('b0b54ee6-ad85-4437-8839-4436a1801bc9')
-
+    // загружаются
     expect(findLoader().exists()).toBe(true)
     expect(findDataError().exists()).toBe(false)
-    expect(findUserDetails().exists()).toBe(false)
+    expect(findUserEdit().exists()).toBe(false)
+    expect(findUserPreview().exists()).toBe(false)
 
-    // ошибка, данные не загрузились
-    expect(findLoader().exists()).toBe(false)
-    expect(findDataError().exists()).toBe(true)
-    expect(findUserDetails().exists()).toBe(false)
+    // ждём первый мок - успех
+    // await flushPromises()
+    //
+    // expect(findLoader().exists()).toBe(false)
+    // expect(findDataError().exists()).toBe(false)
+    // expect(findUserEdit().exists()).toBe(false)
+    // expect(findUserPreview().exists()).toBe(true)
 
-    // данные успешно загрузились
-    expect(findLoader().exists()).toBe(false)
-    expect(findDataError().exists()).toBe(false)
-    expect(findUserDetails().exists()).toBe(true)
+    // ждём второй мок - неудача
+    // await flushPromises()
+    //
+    // expect(findLoader().exists()).toBe(false)
+    // expect(findDataError().exists()).toBe(true)
+    // expect(findUserEdit().exists()).toBe(false)
+    // expect(findUserPreview().exists()).toBe(false)
   })
 })
