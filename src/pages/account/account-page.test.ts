b8c4faffc543e0d3c6cc49a7bf867c74f58af4ef
Natalya_semka <seraya.93@inbox.ru>
2022-09-10 15:58:23 +0300

    Компонент отдельно

--- src/pages/account/account-page.test.ts
+++ src/pages/account/account-page.test.ts
@@ -6,94 +6,11 @@ import {
 } from '@vue/test-utils'
 
 import { describe, expect, it, vi, afterEach } from 'vitest'
-
-// Типы
 import { User } from '~/types/main'
-
-// Компоненты
-// import AccountPage from '~/pages/account/account-page'
-import UserPreview from '~/components/user/user-preview.vue'
-import UserEdit from '~/components/user/user-edit.vue'
-import { ref, computed, Ref, onMounted } from 'vue'
-
-// API и утилиты
+import AccountPage from '~/pages/account/account-page.vue'
 import usersApi from '~/api/users'
-import notify from '~/plugins/notify'
 
 enableAutoUnmount(afterEach)
-
-const AccountPage = {
-  components: {
-    UserPreview,
-    UserEdit
-  },
-  setup() {
-    const isEditing = ref(false)
-
-    // id пользователя из стора
-    const id = '4905bc62-cfdb-40e7-b9b8-15de8b83f888'
-
-    // composable
-    const useUser = (id: string) => {
-      const isLoading = ref(true)
-      const user: Ref<User | null> = ref(null)
-
-      const isUserEmpty = computed(() => {
-        return !user.value
-      })
-
-      const loadUser = () => {
-        isLoading.value = true
-
-        usersApi
-          .loadUser(id)
-          .then((userResponse) => (user.value = userResponse))
-          .catch((error) => notify.error(error))
-          .finally(() => (isLoading.value = false))
-      }
-
-      const updateUser = () => {
-        return true
-      }
-
-      onMounted(() => {
-        loadUser()
-      })
-
-      return {
-        isLoading,
-        user,
-        isUserEmpty,
-        loadUser,
-        updateUser
-      }
-    }
-
-    const { isLoading, user, isUserEmpty, updateUser } = useUser(id)
-
-    return {
-      isLoading,
-      user,
-      isUserEmpty,
-      updateUser,
-      isEditing
-    }
-  },
-  template: `
-    <div class="account-page">
-    <div v-if="isLoading" class="ld-loader">Загрузка</div>
-
-    <template v-else>
-      <div v-if="isUserEmpty" class="data-error">
-        Данные не загрузились. Попробуйте обновить страницу
-      </div>
-
-      <user-edit v-else-if="isEditing" :user="user" @update="updateUser" />
-      <user-preview v-else :user="user" @update="updateUser" />
-    </template>
-    </div>
-  `
-}
 
 const componentFactory = () => {
   return shallowMount(AccountPage)
